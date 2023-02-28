9a9f492ff78f86b79a3e0f6d2bcbf29c3df0949f
NickMarinade <59340748+NickMarinade@users.noreply.github.com>
2023-02-28 14:12:13 +0100

    adding post route

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
 const pool = mariadb.createPool({
     host: process.env.DB_HOST,
@@ -49,9 +49,59 @@ app.get("/:id", async (req, res) => {
     } finally {
         if(connection) connection.end();
     }
+});
+
+app.post("/add", async (req, res) => {
+    let connection;
+    try {
+        connection = await pool.getConnection();
+        const {title, description} = req.body;
+        if (!title || !description) {
+            throw new Error("title and description are required");
+        }
+        await connection.query(
+            `INSERT INTO brilliant_minds.ideas (title, description) VALUES (?, ?)`,
+            [title, description]
+        );
+        res.status(200).send("Idea added successfully");
+    } catch (err) {
+        throw err;
+    } finally {
+        if(connection) connection.end();
+    }
 })
+
 
 app.listen(3000, () => {
     console.log("Server started on http://127.0.0.1:3000");
 });
 
+
+// const insertIdea = async (title, description) => {
+//     let connection; 
+//      try {
+//        connection = await pool.getConnection();
+//        const query = `INSERT INTO Ideas (Title, Description, Created_at) VALUES (?, ?, NOW())`; 
+//        const data = [title, description]; 
+//        const result = await connection.query(query, data); 
+//        console.log(result)
+//        return result; 
+//      } catch (err) {
+//        throw err; 
+//      } finally {
+//        if (connection) connection.end(); 
+//    };
+//    }
+   
+//    app.post("/new-idea", async (req, res) => {
+//      const { Title, Description } = req.body; 
+//      try {
+//        const result = await insertIdea(Title, Description);
+//        res.redirect("http://localhost:5500/client"); //<--- should redirect???
+//      } catch (err) {
+//        console.error(err);
+//        res
+//          .status(500)
+//          .send("Erreur lors de l'insertion de l'idée dans la base de données"); 
+//      }
+//    });
